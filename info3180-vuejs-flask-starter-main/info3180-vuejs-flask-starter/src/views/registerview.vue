9c233cb01f3c8356740d11a9d14c68f845c7ae11
CoderTaj <tajfanus@gmail.com>
2023-04-26 22:02:04 -0500

    registration page

---
+++ info3180-vuejs-flask-starter-main/info3180-vuejs-flask-starter/src/views/registerview.vue
@@ -0,0 +1,34 @@
+<template>
+  <form>
+    <h3>Register</h3>
+
+    <label>Username</label>
+    <input type="username" required v-model="username" />
+
+    <label>Password</label>
+    <input type="password" required v-model="password" />
+
+    <label>Firstname</label>
+    <input type="firstname" required v-model="firstname" />
+
+    <label>Lastname</label>
+    <input type="lastname" required v-model="lastname" />
+
+    <label>Email</label>
+    <input type="email" required v-model="email" />
+
+    <label>Location</label>
+    <input type="location" required v-model="location" />
+
+    <label>Biography</label>
+    <input type="biography" required v-model="biography" />
+
+    <label>Photo</label>
+    <input type="file" @change="onFileSelected" />
+
+    <div class="submit">
+      <button type="submit">Register</button>
+    </div>
+  </form>
+</template>
+
